95cbe348249d2b40ec1a9a8f63ac9b0ae91f1694
alvawinsa <101797878+alvawinsa@users.noreply.github.com>
2025-08-24 14:06:49 +0200

    adds documentation for fact sessions

---
+++ models/gold/core/fct_sessions.sql
@@ -0,0 +1,26 @@
+with events as (select * from {{ ref("stg_events") }}),
+
+/* I would rename these to more sensible names if I had more context to what exactly they corresponded to */
+with final as (
+    select
+        md5(concat_ws(ga_session_id, device_id, install_id)) as session_id,
+        min(case when event_name = 'session_start' then event_timestamp end) as session_start_at,
+        max(event_timestamp) as session_end_at,
+        unix_timestamp(max(event_timestamp)) - unix_timestamp(min(case when event_name = 'session_start' then event_timestamp end)) as session_duration_seconds,
+        device_id,
+        install_id,
+        install_source,
+        max(case when event_name = 'store_impression' then true else false end) as has_store_impression,
+        max(case when event_name = 'app_crashed' then true else false end) as has_app_crashed,
+        max(case when event_name = 'store_entry' then true else false end) as has_entered_store,
+        max(case when event_name = 'in_app_purchased' then true else false end) as has_in_app_purchased,
+        max(case when session_engaged = true then true else false end) as is_session_engaged,
+        max(case when subscription = true then true else false end) as has_subscription
+    from events
+    group by all
+    )
+
+select *
+from final
+where session_start_at is not null -- filter out all non-sessions
+    and session_id is not null -- if we can't identify them it's not useful
