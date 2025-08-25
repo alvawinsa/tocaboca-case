f210bce2f555086d11b81b3e353bc846b19393c6
alvawinsa <101797878+alvawinsa@users.noreply.github.com>
2025-08-25 17:53:29 +0200

    splits out int model

--- models/gold/core/dim_device.sql
+++ models/gold/core/dim_device.sql
@@ -1,18 +1,32 @@
 with events as (select * from {{ ref("stg_events") }}),
 
-final as (
-    select
+    int_purchases as (
+        select
+            device_id,
+            sum(price_usd) as total_usd_spent,
+            sum(quantity) as total_products,
+            true as has_purchased
+        from {{ref("int_purchases")}}
+        group by all
+    ),
+
+    final as (
+        select
         /* there are probably cleaner ways of doing this, e.g. mapping early events install_id to later
         events install_id where device_id is populated */
-        coalesce(device_id, install_id) as device_id,
-        max_by(device_category, event_timestamp) as device_category,
-        max_by(install_source, event_timestamp) as install_source,
-        from_utc_timestamp(min(event_timestamp), 'Europe/Stockholm') as first_event_at_local,
-        from_utc_timestamp(max(event_timestamp), 'Europe/Stockholm') as last_event_at_local,
-        max(ga_session_number) as number_of_sessions,
-        coalesce(max(subscription), false) as has_subscription
-    from events
-    group by all
-)
+            coalesce(device_id, install_id) as device_id,
+            max_by(device_category, event_timestamp) as device_category,
+            max_by(install_source, event_timestamp) as install_source,
+            from_utc_timestamp(min(event_timestamp), 'Europe/Stockholm') as first_event_at_local,
+            from_utc_timestamp(max(event_timestamp), 'Europe/Stockholm') as last_event_at_local,
+            max(ga_session_number) as number_of_sessions,
+            coalesce(max(subscription), false) as has_subscription,
+            coalesce(sum(total_usd_spent), 0) as total_usd_spent,
+            coalesce(sum(total_products), 0) as total_products,
+            coalesce(has_purchased, false) as has_purchased
+        from events
+        left join int_purchases using (device_id)
+        group by all
+    )
 
 select * from final
