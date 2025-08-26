c4bfd27c5800a04d2a27edfff3f1a901484b575c
alva <101797878+alvawinsa@users.noreply.github.com>
2025-08-26 09:55:41 +0200

    Update dim_device.sql

--- models/gold/core/dim_device.sql
+++ models/gold/core/dim_device.sql
@@ -15,6 +15,7 @@ with events as (select * from {{ ref("stg_events") }}),
             device_id,
             max_by(device_category, event_timestamp) as device_category,
             max_by(install_source, event_timestamp) as install_source,
+            /* note this is not the same as their first session, I'd probably add that too */
             from_utc_timestamp(min(event_timestamp), 'Europe/Stockholm') as first_event_at_local,
             from_utc_timestamp(max(event_timestamp), 'Europe/Stockholm') as last_event_at_local,
             max(ga_session_number) as number_of_sessions,
